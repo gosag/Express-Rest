b568f106d52645ec8929225fb847058c32805531
Gosa Girma <gosagirma441@gmail.com>
2026-02-11 08:23:41 +0300

    make the posts to be displayed as a list

--- public/client.js
+++ public/client.js
@@ -1,5 +1,22 @@
 const form = document.querySelector('#postForm');
 const titleInput = document.querySelector('#title');
+const getPostsBtn=document.querySelector('#getPosts');
+const responseDiv=document.querySelector('#response');
+responseDiv.innerHTML="";
+const getPostsHandler=async ()=>{
+    try{
+        const res=await fetch("http://localhost:8000/api/posts");
+        const data=await res.json();
+        responseDiv.innerHTML=(`
+            <h2>Posts:</h2>
+            <ul>
+                ${data.map(post=>`<li>${post.title}</li>`).join('')}
+            </ul>`);
+    }
+    catch(error){
+        console.error("Error fetching posts:",error);
+    }
+}
 const submitHandler=async (e)=>{
     e.preventDefault();
     const res=await fetch("http://localhost:8000/api/posts",{
@@ -9,18 +26,7 @@ const submitHandler=async (e)=>{
         },
         body:JSON.stringify({title:titleInput.value}),
     })
+    getPostsHandler();
 }
 form.addEventListener('submit', submitHandler)
-const getPostsBtn=document.querySelector('#getPosts');
-const responseDiv=document.querySelector('#response');
-const getPostsHandler=async ()=>{
-    try{
-        const res=await fetch("http://localhost:8000/api/posts");
-        const data=await res.json();
-        responseDiv.innerHTML=JSON.stringify(data);
-    }
-    catch(error){
-        console.error("Error fetching posts:",error);
-    }
-}
 getPostsBtn.addEventListener('click',getPostsHandler)
